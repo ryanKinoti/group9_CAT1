7974421f65565c678d13cc28942dcce3cef09bd4
Ryan Kinoti <ryankinotikathurima@gmail.com>
2023-10-04 09:09:39 +0300

    completed: download bug fixed, proper return for dataset path

--- dataset_loader.py
+++ dataset_loader.py
@@ -1,43 +1,58 @@
 import os
 import shutil
-import json
-import logging
 import requests
 from logs_settings import *
 
 
-def fetch_and_prepare_dataset(project_folder, extracted_folder):
-    # a check to see if the extraction was done
-    if os.path.exists(extracted_folder):
-        logger.warning(f"The folder {extracted_folder} already exists. Skipping extraction.")
+def fetch_and_prepare_dataset(project_folder):
+    # a check to see if the download and extraction was done
+    jsonl_folders = set()
+    for root, dirs, files in os.walk(project_folder):
+        for file in files:
+            if file.endswith(".jsonl"):
+                jsonl_folders.add(root)
+
+    if jsonl_folders:
+        logger.warning(
+            f"The folder(s) {', '.join(jsonl_folders)} already exists and contains .jsonl files. Skipping extraction.")
+
+        jsonl_folders_list = list(jsonl_folders)
+        first_folder = jsonl_folders_list[0]
+        # logger.info(f'The datasets are in {first_folder}')
+        return first_folder
     else:
         try:
             tmp_dir = os.path.join(project_folder, 'tmp')
             os.makedirs(tmp_dir, exist_ok=True)
 
-            # downloading the dataset
-            logger.info(f'Downloading the Dataset')
+            logger.info(f'Downloading the Dataset . . . . . .')
             dataset_url = 'https://amazon-massive-nlu-dataset.s3.amazonaws.com/amazon-massive-dataset-1.1.tar.gz'
             response = requests.get(dataset_url)
 
             with open(os.path.join(tmp_dir, "amazon-massive-dataset-1.1.tar.gz"), "wb") as f:
                 f.write(response.content)
 
-            logger.info(f'Unzipping the downloaded Dataset')
+            logger.info(f'Unzipping the downloaded Dataset . . . . .')
             shutil.unpack_archive(os.path.join(tmp_dir, "amazon-massive-dataset-1.1.tar.gz"),
                                   extract_dir=project_folder)
 
-            # get the path of the extracted dataset i.e. where the jsonl files are located
-            jsonl_files = []
+            # fetching the dataset folder
+            jsonl_folders = set()
             for root, dirs, files in os.walk(project_folder):
                 for file in files:
                     if file.endswith(".jsonl"):
-                        print(os.path.join(root, file))
+                        jsonl_folders.add(root)
+
+            jsonl_folders_list = list(jsonl_folders)
+            first_folder = jsonl_folders_list[0]
+            logger.info(f'The datasets are in {first_folder}')
+
+            print('\n')
 
             logger.warning(f'Removing the {tmp_dir} folder')
             os.remove(os.path.join(tmp_dir, "amazon-massive-dataset-1.1.tar.gz"))
             os.rmdir(tmp_dir)
-
-            logger.info(f'Dataset downloading and extraction complete')
+            logger.info(f'Dataset downloading and extraction complete!!')
+            return first_folder
         except Exception as e:
             logger.error(f'An error: "{e}" has occurred')
--- main.py
+++ main.py
@@ -2,10 +2,9 @@ from dataset_loader import *
 
 
 def main():
+    # dataset download and extraction
     project_folder = os.getcwd()
-    extracted_folder = os.path.join(project_folder, '1.1')
-
-    fetch_and_prepare_dataset(project_folder, extracted_folder)
+    fetch_and_prepare_dataset(project_folder)
 
 
 if __name__ == "__main__":
