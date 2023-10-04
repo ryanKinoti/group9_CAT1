3d0c53a5afa3d630dbb809d8b28e91765c19476e
Ryan Kinoti <ryankinotikathurima@gmail.com>
2023-10-04 08:46:32 +0300

    completed, dataset downloading and extraction

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-# all scripts are called here and executed from main
+from dataset_loader import *
+
+
+def main():
+    project_folder = os.getcwd()
+    extracted_folder = os.path.join(project_folder, '1.1')
+
+    fetch_and_prepare_dataset(project_folder, extracted_folder)
+
+
+if __name__ == "__main__":
+    main()
